e7b7593221b324af0c4a28a157485f77c9eba30e
Prince <vasukashyap08@gmail.com>
2023-12-21 20:59:07 +0530

    commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// hamburger 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -6,4 +7,8 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu(){
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-}
+};
+
+// Footer-year 
+const Year = document.querySelector("#current-year")
+Year.innerHTML = new Date().getFullYear();
